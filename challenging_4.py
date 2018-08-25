8c751a87e1490296172abc84c10960bc7f710531
Rummage777 <777.free.writer@gmail.com>
2018-08-25 22:21:49 +0300

    Question 4 solution

---
+++ challenging_4.py
@@ -0,0 +1,53 @@
+# !usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Question:
+Write a program which accepts a sequence of comma-separated numbers
+from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+'''
+
+# Using input from string not console
+input_data = '34,67,55,33,12,98'
+
+
+def list_converter():
+    data_list = input_data.split(',')
+    return data_list
+
+
+def tuple_converter():
+    data_tuple = tuple(list_converter())
+    return data_tuple
+
+
+print(list_converter())
+print(tuple_converter())
+
+
+def test_result():
+    passed = True
+    result = list_converter()
+    if len(result) != 6:
+        passed = False
+        print('Test #1 failed')
+    if result != ['34', '67', '55', '33', '12', '98']:
+        passed = False
+        print('Test #2 failed')
+    number = tuple_converter()
+    if len(number) != 6:
+        passed = False
+        print('Test #3 failed')
+    if number != ('34', '67', '55', '33', '12', '98'):
+        passed = False
+        print('Test #4 failed')
+    if passed:
+        print('Tests are passed')
+
+
+test_result()
