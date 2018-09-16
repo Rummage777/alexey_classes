d6e46585e65e08653b80d6f124c6c4eb3b762478
Rummage777 <777.free.writer@gmail.com>
2018-09-16 21:52:08 +0300

    Question #21 Solution

---
+++ challenging_21.py
@@ -0,0 +1,39 @@
+# !usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from math import sqrt
+
+'''
+A robot moves in a plane starting from the original point (0,0).
+The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps.
+The trace of robot movement is shown as the following:
+UP 5 DOWN 3 LEFT 3 RIGHT 2
+The numbers after the direction are steps. Please write a program
+to compute the distance from current position after a sequence
+of movement and original point. If the distance is a float,
+then just print the nearest integer.
+Example:
+If the following tuples are given as input to the program
+then, the output of the program should be: 2
+'''
+
+commands = {'UP': 5, 'DOWN': 3, 'LEFT': 3, 'RIGHT': 2}
+
+
+def calculate_distance(data):
+    distance = 0
+    location = [0, 0]
+    for item in data:
+        if item == 'UP':
+            location[1] += int(data[item])
+        if item == 'DOWN':
+            location[1] -= int(data[item])
+        if item == 'RIGHT':
+            location[0] += int(data[item])
+        if item == 'LEFT':
+            location[0] -= int(data[item])
+    distance = int(round(sqrt(location[0]**2 + location[1]**2)))
+    return distance
+
+
+print(calculate_distance(commands))
