b4f93f72544a59f1dac892306e2e87b92e024b0e
Rummage777 <777.free.writer@gmail.com>
2018-10-02 03:41:13 +0300

    Question #34 Solution

---
+++ challenging_34.py
@@ -0,0 +1,21 @@
+# !usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from __future__ import print_function
+
+'''
+Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. The function should just print the values only.
+'''
+
+numbers = 20
+
+
+def create_dict(n):
+    new_dict = {}
+    for item in range(1, n+1):
+        new_dict[item] = item ** 2
+        print(new_dict[item], end=' ')
+    return new_dict
+
+
+create_dict(numbers)
