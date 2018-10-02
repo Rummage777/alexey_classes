2076b1846b4c53b61ad134368d8739c26436faf7
Rummage777 <777.free.writer@gmail.com>
2018-10-02 03:42:51 +0300

    Question #38 Solution

---
+++ challenging_38.py
@@ -0,0 +1,18 @@
+# !usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+'''
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print the last 5 elements in the list.
+'''
+
+numbers = 20
+
+
+def create_list(n):
+    new_list = [x**2 for x in range(1, n+1)]
+    return new_list
+
+
+result = create_list(numbers)
+print(result[-5:])
